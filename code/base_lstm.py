c726c971024dae13c397e2e095ea7b9a219f2fa2
esteng <elias.stengel@gmail.com>
2017-12-15 12:11:50 -0500

    unified data

--- code/base_lstm.py
+++ code/base_lstm.py
@@ -1,27 +1,24 @@
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM
-from keras.layers.wrappers import Bidirectional, TimeDistributed
-from keras.regularizers import l2
-
-
-from data import process_csv, process_file, make_x_y
+import argparse 
 import numpy as np 
 import math
 
-
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM
+from keras.layers.wrappers import Bidirectional, TimeDistributed
+# from keras.regularizers import l2
 
 
 np.random.seed(100)
 
-def define_model(embedding_size, tag_size):
+def define_model(embedding_size, tag_size, input_length, embedding = False, vocab_dim = 0):
     model = Sequential()
-    model.add(Bidirectional(LSTM(128, return_sequences=True,
-                                        W_regularizer=l2(0.000), \
-                                        U_regularizer=l2(0.000)), 
-                                                    input_shape=(29,300)))
+    if embedding:
+        model.add(Embedding(vocab_dim, embedding_size, input_length=input_length))
+
+    model.add(Bidirectional(LSTM(128, return_sequences=True),input_shape=(input_length,embedding_size)))
+                                                    
     model.add(Dropout(0.5))
-    model.add(TimeDistributed(Dense(10, activation='softmax',W_regularizer=l2(0.000), \
-                                        b_regularizer=l2(0.000))))
+    model.add(Dense(tag_size, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
               optimizer='adam',
@@ -30,42 +27,8 @@ def define_model(embedding_size, tag_size):
     return model
 
 
-print("loading data")
-# data = process_csv("../data/entity-annotated-corpus/ner_dataset.csv")
-data = process_file("../data/news_tagged_data.txt")
-
-# print("getting X Y sets")
-X, Y, word_embeddings, tag_embeddings = make_x_y(data, 300, True)
 
 
-
-train, test, dev = .7, .2, .1
-
-train_split = int(len(X)*train)
-test_split = train_split+1+int(math.floor(len(X)*test))
-
-
-X_train, Y_train = X[:train_split], Y[:train_split]
-X_test, Y_test = X[train_split +1 : test_split], Y[train_split + 1:test_split]
-X_dev, Y_dev = X[test_split +1 :], Y[test_split +1 :]
-
-
-print("data shape:")
-print("train: {}, test: {}, dev: {}".format(X_train.shape, X_test.shape, X_dev.shape))
-
-
-# tag_to_vec, vec_to_tag = tag_embeddings
-# tag_length = len(tag_to_vec.values())
-tag_length = 10
-word_embedding_size = 300
-print("defining model...")
-model = define_model(word_embedding_size, tag_length)
-print "model summary:"
-print model.summary()
-print("training...")
-model.fit(X_train, Y_train, epochs=20, batch_size=32, verbose=1,  shuffle=True)
-scores = model.evaluate(X_dev, Y_dev, verbose=1)
-model.validate(X_dev, Y_dev)
 
 
 
--- code/data.py
+++ code/data.py
@@ -1,153 +1,169 @@
 import csv 
 import re
-import gensim
 import numpy as np
 import pandas as pd
 from gensim.models.keyedvectors import KeyedVectors
+from nltk.corpus import conll2002
+import nltk
 
-# from keras.preprocessing.sequence import pad_sequences
+class DataLoader(object):
+    """docstring for DataLoader"""
+    def __init__(self, arg):
+        super(DataLoader, self).__init__()
+        self.X_train = None
+        self.Y_train = None
+        self.X_test = None
+        self.Y_test = None
+        self.X_dev = None
+        self.Y_dev = None
+        self.tag_length = None
+        self.w2v_size = None
+        self.max_length =None
 
+    def get_file_data(self, path):
+        print("loading data")
+        if args.input_path.endswith(".csv"):
+            data = process_csv(args.input_path)
+            file = False
+        else:
+            data = process_file(args.input_path)
+            file = True
 
-def process_csv(path):
-    with open(path) as f1:
-        reader = csv.DictReader(f1)
-        sent = None
-        to_ret = []
-        # add sentence info
-        for i, row in enumerate(reader):
-            if row["Sentence #"] is not None:
-                sent = row["Sentence #"]
-            else:
-                row["Sentence #"] = sent
+        # print("getting X Y sets")
+        X, Y, word_embeddings, tag_embeddings, max_length = make_x_y(data, args.embedding_path, file)
+        self.w2v_size = len(word_embeddings[list(word_embeddings.keys())[0]])
+        train, test, dev = .7, .2, .1
 
-            to_ret.append(row)
-        return to_ret
+        train_split = int(len(X)*train)
+        test_split = train_split+1+int(math.floor(len(X)*test))
 
-def process_file(path):
-    with open(path) as f1:
-        file = f1.read()
-    line_sentences = re.split("(?<![\wO])\n", file)
-    print("there are {} sentences".format(len(line_sentences)))
-    line_sentences = [re.split("\n", x) for x in line_sentences]
-    line_sentences = [[re.split("\s", word_pair) for word_pair in sent ] for sent in line_sentences]
+        self.X_train, self.Y_train = X[:train_split], Y[:train_split]
+        self.X_test, self.Y_test = X[train_split +1 : test_split], Y[train_split + 1:test_split]
+        self.X_dev, self.Y_dev = X[test_split +1 :], Y[test_split +1 :]
 
-    return line_sentences
+        print("data shape:")
+        print("train: {}, test: {}, dev: {}".format(X_train.shape, X_test.shape, X_dev.shape))
 
+        self.tag_length = len(tag_embeddings[0].keys())
 
+    # def load_conll(self, version):
+    #     train=conll2002.sents(version+'.train')
+    #     dev=conll2002.sents(version+'.testa')
+    #     test=conll2002.sents(version+'.testb')
+    #     print(dev)
+    #     # return train, dev, test
 
-def get_all_sentences(data, file=False):
-    all_sents = []
-    sentence = []
-    if not file:
-        current_sent = data[0]["Sentence #"]
-        for i, row in enumerate(data): 
-            if current_sent == row["Sentence #"]:
-                sentence.append((row["Word"], row["Tag"]))
-            else:
-                all_sents.append(sentence)
-                sentence = [(row["Word"], row["Tag"])]
-                current_sent = row["Sentence #"]
+    def process_csv(self, path):
+        with open(path) as f1:
+            reader = csv.DictReader(f1)
+            sent = None
+            to_ret = []
+            sentence = []
+            tag_sequence = []
+            # add sentence info
+            for i, row in enumerate(reader):
+                if row["Sentence #"] is not "":
+                    to_ret.append((sentence, tag_sequence))
+                    sentence, tag_sequence = [], []
+                else:
+                    sentence.append(row["Word"])
+                    tag_sequence.append(row["Tag"])
 
-    if file:
-        all_sents = [[pair for pair in sent if len(pair)>1 ] for sent in data]
-    return all_sents
+            return to_ret[1:]
 
-def make_x_y(data, size, file=False):
-    all_sentences = get_all_sentences(data, file)
+    def process_file(self, path):
+        with open(path) as f1:
+            file = f1.read()
+        line_sentences = re.split("(?<![\wO])\n", file)
+        line_sentences = [re.split("\n", x) for x in line_sentences]
+        line_sentences = [[re.split("\s", word_pair) for word_pair in sent ] for sent in line_sentences]
 
-    #shuffle
-    # np.random.shuffle(all_sentences)
+        return line_sentences
 
-    all_sent_len = [len(x) for x in all_sentences]
-    just_sents = [[y[0] for y in x] for x in all_sentences]
-    max_len = max(all_sent_len)
-    print("Max sentence length is : {}".format(max_len))
-    # w2v_mapping = gensim.models.Word2Vec(just_sents, min_count=1, size = size)
-    # model.save("testmodel.wv")
-    # sys.exit()
-    print "loading w2v model"
-    w2v_mapping = gensim.models.KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin", binary=True)
-    # model = gensim.models.KeyedVectors.load_word2vec_format("wordvecs.txt", binary=False)
-    # wordvecs = pd.read_table("wordvecs.txt", sep='\t', header=None)
-    # print(wordvecs)
-    # print(wordvecs.drop(wordvecs.columns[[-1]], axis=1))
-    # w2v_mapping = {}
-    # with open("wordvecs.txt") as f1:
-    #     lines = f1.readlines()
-    #     for line in lines:
-    #         splitline = line.strip().split("\t")
-    #         word = splitline [0]
-    #         vector = np.array([x for x in splitline[1:] if x is not None])
-    #         w2v_mapping[word] = vector
+    def get_all_sentences(self, data, file=False):
+        all_sents = []
+        sentence = []
+        if not file:
+            all_sents = [zip(sent[0], sent[1]) for sent in data]
 
+        if file:
+            all_sents = [[pair for pair in sent if len(pair)>1 ] for sent in data]
+        return all_sents
 
-    # w2v_vocab = w2v_mapping.keys()
-    # w2v_vocab = w2v_mapping.vocab
-    # get all words 
-    all_words = set([tup[0] for sent in all_sentences for tup in sent])
-    # get oov 
-    # oovs = all_words - set(w2v_vocab.keys())
-    # print "there are {} oovs".format(len(oovs))
-    # # extend w2v by adding random vectors for oovs
-    oov_dict = {}
-    for oov in oovs:
-        oov_vec = np.random.normal(0,1, 300)
-        oov_dict[oov] = oov_vec/np.sum(oov_vec)
+    def make_x_y(self, data, w2v_path=None, file=True):
+        # check if .csv or .txt
+        all_sentences = get_all_sentences(data, file)
+        print(all_sentences[0])
+        all_sent_len = [len(x) for x in all_sentences]
+        just_sents = [[y[0] for y in x] for x in all_sentences]
+        max_len = max(all_sent_len)
 
-    if not file:
-        all_tags = sorted(list(set([row["Tag"] for row in data])))
-    if file:
+        if w2v_path is not None:
+            print "loading w2v model"
+            try:
+                w2v_mapping = gensim.models.KeyedVectors.load_word2vec_format(w2v_path, binary=True)
+            except:
+                w2v_mapping = gensim.models.KeyedVectors.load_word2vec_format(w2v_path, binary=False)
+            
+            size = len(w2v_mapping[list(w2v_mapping.vocab.keys())[0]])
+            w2v_vocab = w2v_mapping.vocab
+            # get all words 
+            all_words = set([tup[0] for sent in all_sentences for tup in sent])
+            # get oov 
+            oovs = all_words - set(w2v_vocab.keys())
+            # # extend w2v by adding random vectors for oovs
+            oov_dict = {}
+            for oov in oovs:
+                oov_vec = np.random.normal(0,1, size)
+                oov_dict[oov] = oov_vec/np.sum(oov_vec)
+
+        else:
+            # if word2vec not used, need to assign each word a separate integer for embedding layer
+            pass
+
+        # if file:
         all_tags = sorted(list(set([pair[1] for sent in all_sentences for pair in sent])))
 
-    all_tags.append("NULL")
-    print("there are {} tags".format(len(all_tags)))
-    tag_to_one_hot = {}
-    one_hot_to_tag = {}
-    for i, tag in enumerate(all_tags):
-        tag_vector = np.zeros((len(all_tags)))
-        tag_vector[i] = 1
-        tag_to_one_hot[tag] = tag_vector
-        one_hot_to_tag[tuple(tag_vector)] = tag
+        all_tags.append("NULL")
+        tag_to_one_hot = {}
+        one_hot_to_tag = {}
+        for i, tag in enumerate(all_tags):
+            tag_vector = np.zeros((len(all_tags)))
+            tag_vector[i] = 1
+            tag_to_one_hot[tag] = tag_vector
+            one_hot_to_tag[tuple(tag_vector)] = tag
 
-    null_embedding = np.zeros((size))
-    null_tag = np.zeros((len(tag_vector)))
-    null_tag[-1] = 1
+        null_embedding = np.zeros((size))
+        null_tag = np.zeros((len(tag_vector)))
+        null_tag[-1] = 1
 
-    tag_to_one_hot["NULL"] = null_tag
-    one_hot_to_tag[tuple(null_tag)]  = "NULL"
-    print "there are this many tags:"
+        tag_to_one_hot["NULL"] = null_tag
+        one_hot_to_tag[tuple(null_tag)]  = "NULL"
+        x_full = []
+        y_full = []
 
-    x_full = []
-    y_full = []
+        for sent in all_sentences:
+            w2v_seq = []
+            tag_seq = []
+            for i, tup in enumerate(sent):
+                word, tag = tup
+                try:
+                    # w2v_seq.append(model[word])
+                    w2v_seq.append(w2v_mapping[word])
+                except KeyError:
+                    w2v_seq.append(oov_dict[word])
+                tag_seq.append(tag_to_one_hot[tag])
+          
+            len_from_max = max_len - len(w2v_seq)
 
-    for sent in all_sentences:
-        w2v_seq = []
-        tag_seq = []
-        for i, tup in enumerate(sent):
-            word, tag = tup
-            try:
-                # w2v_seq.append(model[word])
-                w2v_seq.append(w2v_mapping[word])
-            except KeyError:
-                w2v_seq.append(oov_dict[word])
-            tag_seq.append(tag_to_one_hot[tag])
-      
-        len_from_max = max_len - len(w2v_seq)
-
-
-        x_full.append(np.array(((len_from_max)*[null_embedding]) + w2v_seq ))
-        y_full.append(np.array(((len_from_max)*[null_tag]) + tag_seq))
+            x_full.append(np.array(((len_from_max)*[null_embedding]) + w2v_seq ))
+            y_full.append(np.array(((len_from_max)*[null_tag]) + tag_seq))
 
 
-    x_full = np.array(x_full)
-    y_full = np.array(y_full)
-    print x_full.shape
-    print y_full.shape
-    return x_full, y_full, w2v_mapping, (tag_to_one_hot, one_hot_to_tag)
-
-
-
-# data = process_csv("../data/entity-annotated-corpus/ner_dataset.csv")
-# X, Y, word_embeddings, tag_embeddings = make_x_y(data, 300)
+        x_full = np.array(x_full)
+        y_full = np.array(y_full)
+        print x_full.shape
+        print y_full.shape
+        return x_full, y_full, w2v_mapping, (tag_to_one_hot, one_hot_to_tag), max_len
 
 
