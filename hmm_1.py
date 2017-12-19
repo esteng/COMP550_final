f3cab6bc5ef4031af5cdfdfce26518e391820724
emulhall <emily.mulhall8@gmail.com>
2017-12-19 15:29:45 -0500

    committing changes before going in and starting a new branch

--- hmm_1.py
+++ hmm_1.py
@@ -3,6 +3,7 @@ import nltk
 from nltk.tag import hmm
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
+from collections import Counter
 
 #train data- pre-tagged
 
@@ -10,7 +11,7 @@ def data_loader(version):
 	train=conll2002.raw(version+'.train')
 	dev=conll2002.raw(version+'.testa')
 	test=conll2002.raw(version+'.testb')
-	return train.encode('ascii', 'ignore'), dev.encode('ascii', 'ignore'), test.encode('ascii', 'ignore')
+	return label(train.encode('ascii', 'ignore')), label(dev.encode('ascii', 'ignore')), label(test.encode('ascii', 'ignore'))
 
 def label(data):
 	line_tokenized=data.splitlines()
@@ -38,9 +39,27 @@ def label(data):
 def tag(version):
 	train_esp, dev_esp, test_esp=data_loader(version)
 	trainer=hmm.HiddenMarkovModelTrainer()
-	train_data=label(train_esp)
-	tagger = trainer.train_supervised(train_data)
-	dev_data=label(dev_esp)
-	tagger.test(dev_data)
+	tagger = trainer.train_supervised(train_esp)
+	tagger.test(dev_esp)
 
-tag('esp')
+def baseline(train, test):
+	tags=[]
+	for sent in train:
+		tags.append([x[1] for x in sent])
+	flat_tags=[y for sublist in tags for y in sublist]
+	commonality=Counter(flat_tags)
+	most_common=commonality.most_common(1)[0][0]
+	print most_common
+	correct=0
+	total=0
+	for sent in test:
+		for word in sent:
+			total+=1
+			if most_common==word[1]:
+				correct+=1
+	return float(correct)/total
+
+
+#tag('esp')
+train_esp, dev_esp, test_esp=data_loader('esp')
+print baseline(train_esp, dev_esp)
